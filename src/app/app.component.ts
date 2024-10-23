396e739345ca3ad445ebcf0df1da9ca6efafa221
Cleison Nunes <cleison.nunes20@gmail.com>
2024-10-23 07:50:44 -0300

    Adicionando servicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FormsComponent } from "./components/forms/forms.component";
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GetUnitsService } from './services/get-units.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FormsComponent],
+  imports: [RouterOutlet, HeaderComponent, FormsComponent, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  
 })
 export class AppComponent {
   title = 'DesafioSmartFit';
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { GetUnitsService } from './../../services/get-units.service';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,HttpClientModule,AppComponent],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
 export class FormsComponent implements OnInit {
+
+httpClient = inject(HttpClient)
+
 results = [];
 formGroup!: FormGroup;
 
-constructor(private formBuilder: FormBuilder){
+constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService){
 
 }
   ngOnInit(): void {
+    this.unitService.getAllUnits().subscribe(data => console.log(data));
     this.formGroup = this.formBuilder.group({
     hour: '',
     showClosed: false,
